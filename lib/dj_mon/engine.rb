1589e4312562a47c6dde9cb33c7e6e1be6e983d7
Akshay Rawat <projects@akshay.cc>
2012-05-22 19:08:04 +0530

    updates

--- lib/dj_mon/engine.rb
+++ lib/dj_mon/engine.rb
@@ -1,6 +1,13 @@
 module DjMon
   class Engine < Rails::Engine
     isolate_namespace DjMon
-    config.asset_path = "/dj_mon%s" # note %s at the end
+    config.asset_path = "/dj_mon%s"
+
+    if Rails.version > "3.1"
+      initializer "DJMon precompile hook" do |app|
+        app.config.assets.precompile += ['dj_mon.js', 'dj_mon.css']
+      end
+    end
+
   end
 end
