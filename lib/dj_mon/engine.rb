c0f3d9d41c842c90e43fa0b591ebb0fd0701823e
Akshay Rawat <projects@akshay.cc>
2012-05-23 00:46:16 +0530

    add notes to read, remove some unnec code

--- lib/dj_mon/engine.rb
+++ lib/dj_mon/engine.rb
@@ -1,7 +1,7 @@
 module DjMon
   class Engine < Rails::Engine
+
     isolate_namespace DjMon
-    config.asset_path = "/dj_mon%s"
 
     if Rails.version > "3.1"
       initializer "DJMon precompile hook" do |app|
